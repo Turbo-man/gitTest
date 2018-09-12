350f67c32ef7d7edb4f85ed8649a9ec262288198
turbo <645268250@qq.com>
2018-09-12 19:20:42 +0800

    create proj

--- src/main/java/com/Book.java
+++ src/main/java/com/Book.java
@@ -16,6 +16,6 @@ import org.springframework.stereotype.Component;
 
 public class Book {
     public void add(){
-        System.out.println("add...cccc");
+        System.out.println("add...ccc");
     }
 }
