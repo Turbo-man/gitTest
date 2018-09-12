7ebd72309577620ae35381e26136cd5b7726d562
turbo <645268250@qq.com>
2018-09-12 17:36:11 +0800

    create proj

---
+++ src/main/java/com/Book.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Copyright © 2018 上海金互行金融服务有限公司. All rights reserved. *
+ *
+ * @Title:
+ * @Prject: first
+ * @Package: com.a
+ * @Description: ${todo}
+ * @author: Turbo@baidu.com
+ * @date: 2018/6/12 12:37
+ * @version: V1.0
+ */
+
+public class Book {
+    public void add(){
+        System.out.println("add...");
+    }
+}
---
+++ src/main/java/com/test.java
@@ -0,0 +1,28 @@
+package com;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Copyright © 2018 上海金互行金融服务有限公司. All rights reserved. *
+ *
+ * @Title:
+ * @Prject: first
+ * @Package: com
+ * @Description: ${todo}
+ * @author: Turbo@baidu.com
+ * @date: 2018/6/11 11:36
+ * @version: V1.0
+ */
+public class test {
+    @Test
+   public void mm(){
+        long b=12121212323323L;
+        int a=(int )b;
+        a+=2L;
+
+        System.out.println(a);
+    }
+    }
+
